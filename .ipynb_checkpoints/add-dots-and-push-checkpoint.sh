2efa145e3be1374246823bb09fa570e43d0b313b
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2026-05-19 05:26:00 +0000

    Auto-push

--- .ipynb_checkpoints/add-dots-and-push-checkpoint.sh
+++ .ipynb_checkpoints/add-dots-and-push-checkpoint.sh
@@ -1,6 +1,8 @@
 # ...JupyterHub doesn't seem to allow viewing dot files/folders in the UI.
 # So, this renames "github" to ".github" before pushing
-cp -r github .github
+rm -rf .github
+mkdir .github
+cp -r ./github/* ./.github/*
 git add -A .
 git commit -m "Auto-push"
 git push

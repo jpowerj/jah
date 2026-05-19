a87fd693276db7a0c393e4616435b27d6067d756
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2026-05-19 05:48:23 +0000

    Auto-push

--- src/jah/__init__.py
+++ src/jah/__init__.py
@@ -1,4 +1,4 @@
-def fetch(assignment_id):
+def fetch_corrections(assignment_id):
   import base64, datetime, json, pytz
   from urllib.request import urlopen
   from IPython.display import display, Markdown
@@ -8,7 +8,7 @@ def fetch(assignment_id):
     md_content = base64.b64decode(md_content_b64).decode('utf-8')
     return md_content
   exec_timestamp = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime("%Y-%m-%d %H:%M:%S %Z")
-  corrections_url = f"https://api.github.com/repos/jpowerj/dsan-content/contents/2025-sum-dsan5650/DSAN5650_{assignment_id}_Corrections.md?ref=main"
+  corrections_url = f"https://api.github.com/repos/jpowerj/dsan-content/contents/2026-sum-dsan5650/DSAN5650_{assignment_id}_Corrections.md?ref=main"
   md_content = fetch_github(corrections_url)
   corrections_content = md_content + f"\n\nLast fetched: {exec_timestamp}"
   display(Markdown(corrections_content))
